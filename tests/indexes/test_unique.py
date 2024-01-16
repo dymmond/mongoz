9afd043350e2d2a643271ff0c8faa88a7566d5b5
tarsil <tiago.arasilva@gmail.com>
2024-01-16 11:17:22 +0000

    Add unique index validation

---
+++ tests/indexes/test_unique.py
@@ -0,0 +1,41 @@
+from typing import AsyncGenerator
+
+import pydantic
+import pytest
+from pymongo.errors import DuplicateKeyError
+from tests.conftest import client
+
+import mongoz
+from mongoz import Document
+
+pytestmark = pytest.mark.anyio
+pydantic_version = pydantic.__version__[:3]
+
+
+class Movie(Document):
+    name: str = mongoz.String(unique=True)
+    is_published: bool = mongoz.Boolean(default=False)
+
+    class Meta:
+        registry = client
+        database = "test_db"
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def prepare_database() -> AsyncGenerator:
+    await Movie.drop_indexes(force=True)
+    await Movie.objects.delete()
+    await Movie.create_indexes()
+    yield
+    await Movie.drop_indexes(force=True)
+    await Movie.objects.delete()
+    await Movie.create_indexes()
+
+
+async def test_unique():
+    movie = await Movie.objects.create(name="Barbie")
+
+    assert movie.is_published is False
+
+    with pytest.raises(DuplicateKeyError):
+        await Movie.objects.create(name="Barbie")
