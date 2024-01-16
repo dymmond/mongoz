b0d6f09e3862a317fd3f1da6e49b0a3840aa4046
tarsil <tiago.arasilva@gmail.com>
2024-01-16 09:44:24 +0000

    Add default tests for boolean

---
+++ tests/models/manager/test_boolean_default.py
@@ -0,0 +1,55 @@
+from typing import AsyncGenerator, List, Optional
+
+import pydantic
+import pytest
+from tests.conftest import client
+
+import mongoz
+from mongoz import Document, Index, IndexType, ObjectId, Order
+
+pytestmark = pytest.mark.anyio
+pydantic_version = pydantic.__version__[:3]
+
+indexes = [
+    Index("name", unique=True),
+    Index(keys=[("year", Order.DESCENDING), ("genre", IndexType.HASHED)]),
+]
+
+
+class Movie(Document):
+    name: str = mongoz.String()
+    year: int = mongoz.Integer()
+    tags: Optional[List[str]] = mongoz.Array(str, null=True)
+    uuid: Optional[ObjectId] = mongoz.ObjectId(null=True)
+    is_published: bool = mongoz.Boolean(default=False)
+
+    class Meta:
+        registry = client
+        database = "test_db"
+        indexes = indexes
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def prepare_database() -> AsyncGenerator:
+    await Movie.drop_indexes(force=True)
+    await Movie.objects.delete()
+    await Movie.create_indexes()
+    yield
+    await Movie.drop_indexes(force=True)
+    await Movie.objects.delete()
+    await Movie.create_indexes()
+
+
+async def test_model_bool_default() -> None:
+    movies = await Movie.objects.all()
+    assert len(movies) == 0
+
+    await Movie.objects.create(name="Forrest Gump", year=2003)
+
+    movies = await Movie.objects.all()
+    assert len(movies) == 1
+
+    cursor = Movie.query()
+    async for movie in cursor:
+        assert movie.name == "Forrest Gump"
+        assert movie.is_published is False
