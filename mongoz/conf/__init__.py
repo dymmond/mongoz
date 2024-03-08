7b01ab0787ced1dba6ff7b295f8733a8ce4a3f57
Tiago Silva <tiago.arasilva@gmail.com>
2024-03-08 11:47:20 +0000

    New Mongoz LazySettings loading (#25)

--- mongoz/conf/__init__.py
+++ mongoz/conf/__init__.py
@@ -1,8 +1,51 @@
+from __future__ import annotations
+
 import os
+from typing import TYPE_CHECKING, Any
 
-os.environ.setdefault("OVERRIDE_SETTINGS_MODULE_VARIABLE", "MONGOZ_SETTINGS_MODULE")
+from mongoz.conf.functional import LazyObject, empty
+from mongoz.conf.module_import import import_string
 
-if not os.environ.get("MONGOZ_SETTINGS_MODULE"):
-    os.environ.setdefault("MONGOZ_SETTINGS_MODULE", "mongoz.conf.global_settings.MongozSettings")
+if TYPE_CHECKING:
+    from mongoz.conf.global_settings import MongozSettings
 
-from dymmond_settings import settings as settings  # noqa
+ENVIRONMENT_VARIABLE = "MONGOZ_SETTINGS_MODULE"
+
+
+class MongozLazySettings(LazyObject):
+    """
+    A lazy proxy for either global Mongoz settings or a custom settings object.
+    The user can manually configure settings prior to using them. Otherwise,
+    Mongoz uses the settings module pointed to by MONGOZ_SETTINGS_MODULE.
+    """
+
+    def _setup(self, name: str | None = None) -> None:
+        """
+        Load the settings module pointed to by the environment variable. This
+        is used the first time settings are needed, if the user hasn't
+        configured settings manually.
+        """
+        settings_module: str = os.environ.get(
+            ENVIRONMENT_VARIABLE, "mongoz.conf.global_settings.MongozSettings"
+        )
+
+        settings: type[MongozSettings] = import_string(settings_module)
+
+        for setting, _ in settings().dict().items():
+            assert setting.islower(), "%s should be in lowercase." % setting
+
+        self._wrapped = settings()
+
+    def __repr__(self: MongozLazySettings) -> str:
+        # Hardcode the class name as otherwise it yields 'MongozSettings'.
+        if self._wrapped is empty:
+            return "<MongozLazySettings [Unevaluated]>"
+        return f'<MongozLazySettings "{self._wrapped.__class__.__name__}">'
+
+    @property
+    def configured(self) -> Any:
+        """Return True if the settings have already been configured."""
+        return self._wrapped is not empty
+
+
+settings: type[MongozSettings] = MongozLazySettings()  # type: ignore
