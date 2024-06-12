24da2755e3fef89cedbe6562c7b94ffbadeb5c9c
tarsil <tiago.arasilva@gmail.com>
2024-06-12 11:12:18 +0100

    Fix flat test

--- tests/models/manager/test_sort_two.py
+++ tests/models/manager/test_sort_two.py
@@ -32,7 +32,7 @@ async def test_model_sort_asc() -> None:
     for i in range(10):
         await Movie.objects.create(idx=i)
 
-    movies = await Movie.objects.sort(idx__asc=True).values_list(["idx"], flatten=True)
+    movies = await Movie.objects.sort(idx__asc=True).values_list(["idx"], flat=True)
 
     assert movies == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -41,7 +41,7 @@ async def test_model_sort_desc() -> None:
     for i in range(10):
         await Movie.objects.create(idx=i)
 
-    movies = await Movie.objects.sort(idx__desc=True).values_list(["idx"], flatten=True)
+    movies = await Movie.objects.sort(idx__desc=True).values_list(["idx"], flat=True)
 
     assert movies == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
@@ -50,7 +50,7 @@ async def test_model_sort_asc_obj() -> None:
     for i in range(10):
         await Movie.objects.create(idx=i)
 
-    movies = await Movie.objects.sort("idx", Order.ASCENDING).values_list(["idx"], flatten=True)
+    movies = await Movie.objects.sort("idx", Order.ASCENDING).values_list(["idx"], flat=True)
 
     assert movies == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -59,6 +59,6 @@ async def test_model_sort_obj() -> None:
     for i in range(10):
         await Movie.objects.create(idx=i)
 
-    movies = await Movie.objects.sort("idx", Order.DESCENDING).values_list(["idx"], flatten=True)
+    movies = await Movie.objects.sort("idx", Order.DESCENDING).values_list(["idx"], flat=True)
 
     assert movies == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
