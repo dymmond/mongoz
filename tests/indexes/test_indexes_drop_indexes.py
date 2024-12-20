25707e102ead39d495b814bc993e5fa4d6604779
Harshali Zode <66934194+harshalizode@users.noreply.github.com>
2024-12-20 17:35:50 +0530

    Add the multi-tenancy for the indexes handling (#64)
    * *Add the multi-tenancy for the handling indexes
    * Add the test cases.
    * Call the delete method.
    * *Drop the index before creating.
    * * Pass the collection to drop index.
    * * prepare the database for test drop indices.
    * * Create new model movie and removed the index tat defined in meta.
    * * Update the prepare database.

--- tests/indexes/test_indexes_drop_indexes.py
+++ tests/indexes/test_indexes_drop_indexes.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import AsyncGenerator, Optional
 
 import pytest
 
@@ -24,6 +24,28 @@ class AnotherMovie(Document):
         indexes = indexes
         database = "test_db"
         autogenerate_index = True
+
+
+class Movie(Document):
+    name: str = mongoz.String()
+    email: str = mongoz.Email(index=True, unique=True)
+    year: int = mongoz.Integer()
+    uuid: Optional[ObjectId] = mongoz.ObjectId(null=True)
+
+    class Meta:
+        registry = client
+        database = "test_db"
+        autogenerate_index = True
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def prepare_database() -> AsyncGenerator:
+    collection = Movie.objects.using("another_test_db")._collection
+    await Movie.drop_indexes(force=True, collection=collection)
+    await Movie.objects.using("another_test_db").delete()
+    yield
+    await Movie.drop_indexes(force=True)
+    await Movie.objects.using("another_test_db").delete()
 
 
 async def test_drops_indexes() -> None:
@@ -62,3 +84,13 @@ async def test_drops_indexes() -> None:
     total_indexes = await AnotherMovie.list_indexes()
 
     assert len(total_indexes) == 1
+
+
+async def test_drops_indexes_different_db() -> None:
+    collection = Movie.objects.using("another_test_db")._collection
+    await Movie.create_indexes(collection=collection)
+    await Movie.objects.using("another_test_db").create(
+        name="Mongoz", email="mongoz@mongoz.com", year=2023)
+    total_indexes = await Movie.list_indexes(collection=collection)
+
+    assert len(total_indexes) == 2
